9f3629f1b5a0a056884befd8abf04d68f6997e37
Koushik Jayanthi <30872050+Koushik999999@users.noreply.github.com>
2026-07-31 11:59:35 +0530

    Add files via upload

---
+++ PayPage.js
@@ -0,0 +1,77 @@
+function log(message) {
+
+    if (window.webkit?.messageHandlers?.payPage) {
+
+        window.webkit.messageHandlers.payPage.postMessage({
+            type: "log",
+            message
+        });
+
+    } else {
+
+        console.log(message);
+
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    log("DOM Loaded");
+
+});
+
+window.initializePayment = function (session) {
+
+    log("Initializing payment page...");
+
+    window.paymentSession = session;
+
+    log("CSIPayJS type = " + typeof CSIPayJS);
+
+    if (!session) {
+
+        log("PaymentSession missing");
+        return;
+
+    }
+
+    try {
+
+        log("Before constructor");
+
+        const csipay = CSIPayJS(session.accessToken);
+
+        log("After constructor");
+
+        log("CSIPayJS initialized");
+
+        const components = csipay.components({
+            orderId: session.orderId
+        });
+
+        log("Components created");
+
+        components.addComponent(
+            "cardElement",
+            "full-card"
+        );
+
+        log("Full Card component added");
+
+    } catch (e) {
+
+        log("Initialization failed");
+
+        log(e.name);
+
+        log(e.message);
+
+        if (e.stack) {
+
+            log(e.stack);
+
+        }
+
+    }
+
+};
